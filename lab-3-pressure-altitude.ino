8a5af7ce47efd6ae89ebc3ecff3c97e899d462a9
EBUJOLD <emilyebujold@gmail.com>
2017-06-19 15:29:41 -0400

    Update lab-3-pressure-altitude.ino
    Changed display font sizes. removed redundant lines of code. added comments. Updated to match code style guide.

--- lab-3-pressure-altitude.ino
+++ lab-3-pressure-altitude.ino
@@ -8,7 +8,7 @@
 #include <xSW01.h>                //This library allows us to use the weather sensor
 
 void setup() {
-  // This setup code will prepare our xSystem for use. It will only run once.
+// This setup code will prepare our xSystem for use. It will only run once.
 
   Wire.begin(2,14);               //Start I2C communication with the xSystem
   OLED.begin();                   //Turn on the OLED Screen
@@ -16,35 +16,31 @@ void setup() {
 
   OD01.set2X();
   OD01.println("===========");    
-  OD01.println("  Weather  ");    //print opening message on OLED Screen
+  OD01.println("  Weather  ");    //Print opening message on OLED Screen
   OD01.println("===========");
 
-  delay(5000);                    //allow SW01 sensor to normalize before starting loop
+  delay(5000);                    //Allow SW01 sensor to normalize before starting loop
 }
 
 void loop() {
-  // This is the body of the code. It will repeat indefinitely.
+// This is the body of the code. It will repeat indefinitely.
+  OD01.clear();             //Clear display
+  OD01.set2X();             //Set font size
+  OD01.println("Weather");  //Display 'Weather' title
 
-  //Display Pressure
-  OD01.clear();                         //Clear the OLED display
-  float pressure = 0;                   //declare pressure variable and initialize as zero
-  SW01.poll();                          //Tell the SW01 sensor to read and store data
-  pressure = (SW01.getPressure()/100);  //set pressure variable equal to the current pressure and convert to hPa
+//Display Pressure
+  OD01.set1X();                        //Set font size
+  SW01.poll();                         //Tell the SW01 sensor to read and store data
   OD01.println("Pressure:");
-  OD01.print(pressure);                 //display the pressure on the OLED screen
+  OD01.print(SW01.getPressure()/100);  //Display the pressure on the OLED screen
   OD01.println(" hPa");
-  delay(2000);                    //pause for two seconds before displaying new reading
-
-
- //Display Altitude
-  OD01.clear();                   //Clear the OLED display
-  float altitude = 0;             //declare altitude variable and initialize as zero
-  SW01.poll();                    //Tell the SW01 sensor to read and store data
-  altitude = (SW01.getAltitude());  //set altitude variable equal to the current altitude reading
+  OD01.println();
+  
+//Display Altitude
   OD01.println("Altitude:");
-  OD01.print(altitude);           //display the altitude on the OLED screen
-  OD01.println(" m");
-  OD01.println("Above Sea");
-  delay(2000);                    //pause for two seconds
+  OD01.print(SW01.getAltitude()); //Display the altitude on the OLED screen
+  OD01.println(" m above the sea");
+
+  delay(5000);                    //Pause for five seconds
 
 }
