724a7b0ea9cc74e557ee516181e3ee416256464d
EBUJOLD <emilyebujold@gmail.com>
2017-06-19 14:38:07 -0400

    Update lab-3-altitude.ino
    Changed logic statement to for loop as per our discussion

--- lab-3-altitude.ino
+++ lab-3-altitude.ino
@@ -1,11 +1,11 @@
 /////////////////////////////////////////////////////
 // This code displays altitude on the OLED screen
-// Written by J. Singh
+// Written by E. Bujold and J. Singh
 /////////////////////////////////////////////////////
 
-#include <xCore.h>                //This library allows us to use the CW01 processor
-#include <xOD01.h>                //This library allows us to use the OD01 screen
-#include <xSW01.h>                //This library allows us to use the weather sensor
+#include <xCore.h>    //This library allows us to use the CW01 processor
+#include <xOD01.h>    //This library allows us to use the OD01 screen
+#include <xSW01.h>    //This library allows us to use the weather sensor
 
 void setup() {
   // This setup code will prepare our xSystem for use. It will only run once.
@@ -16,46 +16,29 @@ void setup() {
 
   OD01.set2X();
   OD01.println("===========");    
-  OD01.println(" Altitude  ");    //print opening message on OLED Screen
+  OD01.println(" Altitude  ");    //Print opening message on OLED Screen
   OD01.println("===========");
 
-
-
-  delay(5000);                    //allow SW01 sensor to normalize before starting loop
-  OD01.clear();
+  delay(5000);                    //Allow SW01 sensor to normalize before starting loop
 }
 
 void loop() {
   // This is the body of the code. It will repeat indefinitely.
 
-  OD01.clear();
+int count = 1; //Declare counting variable, used in 'for loop'
 
+OD01.clear();               //Refresh display
+OD01.set2X();               //Set font size
+OD01.println("Altitude:");  //Print header on LED
 
- //Display Altitude
-  float altitude1 = 0;              //declare altitude variable and initialize as zero
-  float altitude2 = 0;              //declare second altitude variable for tracking change
-  SW01.poll();                      //Tell the SW01 sensor to read and store data
-  altitude1 = (SW01.getAltitude()); //set pressure variable equal to the current altitude reading
-  delay(500);
-  SW01.poll();                      //repeat for second variable
-  altitude2 = (SW01.getAltitude());
-
-  float diff= abs(altitude2 - altitude1);   //track change over short period of time
-
-   if(diff>1){                              //if reading spikes very quickly, someone is probably blowing on the sensor
-    OD01.println("Altitude");
-    OD01.print(altitude2);
-    OD01.println(" m");
-    OD01.println("It's windy!");
+//Display Altitude
+  for (count = 1; count < 6; count++){  
+    //'For Loop', if count is less than 6, run the loop, then add 1 to count
+    OD01.set1X();                     //Set font size
+    SW01.poll();                      //Get sensor reading
+    OD01.print(SW01.getAltitude());   //Display sensor reading
+    OD01.println(" m");               //Print units
+    delay(2000);                      //Pause for two seconds then repeat
   }
-  else{
-    OD01.println("Altitude");
-    OD01.print(altitude2);
-    OD01.println(" m");
-  }
-
-
-
-  delay(1000);                    //pause for one second then repeat
 
 }
